52b0790475006199d98374db7d5bbe415ac44259
mkruczek <kruczek86@gmail.com>
2021-11-30 12:42:56 +0100

    add simple ws

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -2,13 +2,23 @@ package handlers
 
 import (
 	"github.com/CloudyKit/jet/v6"
+	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
+	"time"
 )
 
-var views = jet.NewSet(
-	jet.NewOSFileSystemLoader("./html"),
-	jet.InDevelopmentMode(),
+var (
+	views = jet.NewSet(
+		jet.NewOSFileSystemLoader("./html"),
+		jet.InDevelopmentMode(),
+	)
+
+	upgradeConnection = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+		CheckOrigin:     func(r *http.Request) bool { return true },
+	}
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
@@ -18,6 +28,35 @@ func Home(w http.ResponseWriter, r *http.Request) {
 		log.Println("ERROR: ", err.Error())
 	}
 
+}
+
+type WsJsonResponse struct {
+	Action      string `json:"action"`
+	Message     string `json:"message"`
+	MessageType string `json:"messageType"`
+}
+
+func WsEndpoint(w http.ResponseWriter, r *http.Request) {
+
+	ws, err := upgradeConnection.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("ERROR: ", err.Error())
+	}
+
+	response := WsJsonResponse{
+		Action:      "do it!",
+		Message:     `<em><small>Connection to the server</small></em>`,
+		MessageType: "html",
+	}
+
+	t := time.Tick(5 * time.Second)
+
+	for range t {
+		err = ws.WriteJSON(response)
+		if err != nil {
+			panic(err)
+		}
+	}
 }
 
 func renderPage(w http.ResponseWriter, tmpl string, data jet.VarMap) error {
